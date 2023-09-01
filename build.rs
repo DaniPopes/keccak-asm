7284dc059d8cc0ee6890cbe947ff28fdce0be42a
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-09-01 02:50:13 +0200

    Add cryptogams, build script

---
+++ build.rs
@@ -0,0 +1,89 @@
+use std::{env, fs, path::Path, process::Command};
+
+// (script, outfile)
+const FILES: &[(&str, &str)] = &[
+    // arm
+    ("cryptogams/arm/keccak1600-armv4.pl", "src/keccak1600-armv4.s"),
+    ("cryptogams/arm/keccak1600-armv8.pl", "src/keccak1600-armv8.s"),
+    // x86
+    ("cryptogams/x86/keccak1600-mmx.pl", "src/keccak1600-x86.s"),
+    // x86_64
+    ("cryptogams/x86_64/keccak1600-avx2.pl", "src/keccak1600-x86_64-avx2.s"),
+    ("cryptogams/x86_64/keccak1600-avx512.pl", "src/keccak1600-x86_64-avx512f.s"),
+    ("cryptogams/x86_64/keccak1600-avx512vl.pl", "src/keccak1600-x86_64-avx512vl.s"),
+    ("cryptogams/x86_64/keccak1600-x86_64.pl", "src/keccak1600-x86_64.s"),
+];
+
+const HEADERS: &[&str] = &["cryptogams/arm/arm_arch.h"];
+
+fn main() {
+    // run Perl scripts
+    if Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/cryptogams")).exists() {
+        for &(script, output) in FILES {
+            rerun_if_changed(script);
+            perl(script, output);
+        }
+    }
+
+    for &path in HEADERS {
+        rerun_if_changed(path);
+        let f = Path::new(path).file_name().unwrap().to_str().unwrap();
+        fs::copy(path, format!("include/{f}")).unwrap();
+    }
+
+    let target_arch = env("CARGO_CFG_TARGET_ARCH");
+    let target_features = env("CARGO_CFG_TARGET_FEATURE");
+    let target_features = target_features.split(',').collect::<Vec<_>>();
+    let feature = |s: &str| target_features.iter().any(|&f| f == s);
+
+    let mut cc = cc::Build::new();
+    cc.include("include");
+    let output = match target_arch.as_str() {
+        "x86" => "src/keccak1600-x86.s",
+        "x86_64" => {
+            if feature("avx512vl") {
+                "src/keccak1600-x86_64-avx512vl.s"
+            } else if feature("avx512f") {
+                "src/keccak1600-x86_64-avx512f.s"
+            } else if feature("avx2") {
+                "src/keccak1600-x86_64-avx2.s"
+            } else {
+                "src/keccak1600-x86_64.s"
+            }
+        }
+        // TODO
+        "aarch64" => {
+            if target_features.iter().any(|&f| f.contains("v8")) {
+                "src/keccak1600-armv8.s"
+            } else {
+                "src/keccak1600-armv4.s"
+            }
+        }
+        s => panic!("Unsupported target arch: {s}"),
+    };
+
+    cc.flag("-c").file(output).compile("keccak1600");
+}
+
+fn perl(path: &str, to: &str) {
+    let mut cmd = Command::new("perl");
+    cmd.arg(path);
+    cmd.args(["elf", to]);
+    let out = cmd.output().unwrap();
+    if !out.status.success() {
+        panic!("perl for {path} failed:\n{}", String::from_utf8_lossy(&out.stderr));
+    }
+    let stdout = String::from_utf8(out.stdout).unwrap();
+    if !stdout.trim().is_empty() {
+        fs::write(to, stdout).unwrap();
+    }
+}
+
+fn rerun_if_changed(path: &str) {
+    println!("cargo:rerun-if-changed={path}");
+}
+
+fn env(s: &str) -> String {
+    println!("cargo:rerun-if-env-changed={s}");
+    env::var(s).unwrap()
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,10 @@
+#![no_std]
 
+#[cfg(not(any(target_arch = "x86_64", target_arch = "x86", target_arch = "aarch64")))]
+compile_error!("crate can only be used on x86, x86-64 and aarch64 architectures");
+
+#[link(name = "keccak1600", kind = "static")]
+extern "C" {
+    fn SHA3_squeeze();
+    fn SHA3_absorb();
+}
